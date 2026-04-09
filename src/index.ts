0d35e50f5b0234c37739e852d14dbc5e8770bf0d
DEV MESSY <techdevask@gmail.com>
2026-04-09 14:06:11 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import { Elysia } from "elysia";
 import { setupMiddleware } from "./middlewares";
 import { setupRoutes } from "./routes";
-import { env } from "./utils/env";
 
 const app = new Elysia();
 setupMiddleware(app);
 setupRoutes(app);
 
-const port = process.env.PORT || env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port);
 
-console.log(
-  `API is running at http://${app.server?.hostname}:${app.server?.port}\nAPI Docs : http://${app.server?.hostname}:${app.server?.port}/swagger`
-);
+console.log(`✅ Mirai Sama API running on port ${port}`);
+console.log(`📚 Swagger: http://localhost:${port}/swagger`);
